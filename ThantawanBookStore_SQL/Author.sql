3188c461f1e499e53d9311dfd92afb2ec2692a15
Kanchanop6509650229 <145246018+Kanchanop6509650229@users.noreply.github.com>
2024-05-03 22:21:10 +0700

    Delete ThantawanBookStore_SQL directory

--- ThantawanBookStore_SQL/Author.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE Author (
-    LessorID CHAR(10) NOT NULL,
-    BookID CHAR(10) NOT NULL,
-    Author VARCHAR(100) NOT NULL,
-    PRIMARY KEY (LessorID, BookID),
-    FOREIGN KEY (LessorID, BookID) REFERENCES Book(LessorID, BookID)
-);
--- ThantawanBookStore_SQL/Category.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE Category (
-    CID CHAR(2) NOT NULL,
-    cname VARCHAR(100) NOT NULL,
-    PRIMARY KEY (CID)
-);
